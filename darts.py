d6b06b66ac74a3dacd41c293af3f798b96ce8945
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-11-04 20:06:20 +0100

    Update darts.py

--- darts.py
+++ darts.py
@@ -1,2 +1,12 @@
 def score(x, y):
-    pass
+    a=((x*x)+(y*y))
+    if (a<(1*1)):
+        return "player earns 10 points"
+    elif (a<(5*5)):
+        return "player earns 5 points"
+    elif (a<(10*10)):
+        return "player earns 1 points"
+    else:
+        return "player earns 0 points"
+    
+    
