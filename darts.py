1aabedc907cbab18bec1fa092882144faa2dfb8d
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-11-04 20:13:11 +0100

    Update darts.py

--- darts.py
+++ darts.py
@@ -1,10 +1,10 @@
 def score(x, y):
     a=((x*x)+(y*y))
-    if (a<(1*1)):
+    if (a<=(1*1)):
         return 10
-    elif (a<(5*5)):
+    elif (a<=(5*5)):
         return 5
-    elif (a<(10*10)):
+    elif (a<=(10*10)):
         return 1
     else:
         return 0
